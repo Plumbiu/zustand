91ec960620f257cc625806745f537648b2579927
Plumbiu <99574369+Plumbiu@users.noreply.github.com>
2023-03-17 22:20:40 +0800

    finished

--- src/store/person.ts
+++ src/store/person.ts
@@ -1,18 +1,20 @@
 import { create } from 'zustand'
-
+import { devtools, persist } from 'zustand/middleware'
 interface IPerson {
   name: string
   age: number
   increase: () => void
 }
 
-const usePersonStore = create<IPerson>()((set) => ({
-  name: 'xj',
-  age: 18,
-  increase: () => {    
-    set((state: any) => ({ age: state.age + 1 }))
-  }
-}))
+const usePersonStore = create<IPerson>()(
+  devtools(
+    persist((set) => ({
+      name: 'xj',
+      age: 18,
+      increase: () => set((state) => ({ age: state.age + 1 }))
+    }), { name: 'person' })
+  )
+)
 
 export const usePersonName = () => usePersonStore(state => state.name)
 export const usePersonAge = () => usePersonStore(state => state.age)
